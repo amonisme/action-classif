89d02555ea4b50b014a5014671ab2e94583862f6
vincent.delaitre <vincent.delaitre@ens-lyon.org>
2010-07-06 15:46:18 +0000

    slides

---
+++ framework/combine_scores.m
@@ -0,0 +1,3 @@
+function [scores, X1, X2] = combine_scores(root_classif1, root_classif2, database) 
+
+end
--- framework/test_BOFLSVM.m
+++ framework/test_BOFLSVM.m
@@ -4,6 +4,6 @@ set_cluster_config;
 global USE_PARALLEL USE_CLUSTER;
 
 USE_PARALLEL = 1;
-USE_CLUSTER = 0;
+USE_CLUSTER = 1;
 
-evaluate(BOFLSVM(1024,1,9), '/data/vdelaitr/FeiFeiNorm', 'feifei_bassoon')
+evaluate(BOFLSVM(1024,1,9), '/data/vdelaitr/DB_FeiFei/DB', 'feifei_bassoon');
