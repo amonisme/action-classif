50982ea6967fcbac7a0f43daa9ba7a8c9d018d60
vincent.delaitre@ens-lyon.org <vincent.delaitre@ens-lyon.org@2912f6c7-716d-e9ba-4c0e-5bf2b11f18db>
2010-08-23 16:22:11 +0000

    display_res

--- trunk/framework/common/matlab/misc/output_info/display_results.m
+++ trunk/framework/common/matlab/misc/output_info/display_results.m
@@ -8,15 +8,27 @@
 
     load(fullfile(root,dir,'results.mat'));
     
-    % Output results
-    if ~isempty(map_sub2sup)        
-        fprintf('Results for subclasses:\n');    
+    % Backward compatibility
+    if ~exist(map_sub2sup)
+        n_img = length(assigned_label);
+        assigned = zeros(n_img, length(classes));
+        correct = zeros(n_img, length(classes));        
+        for i=1:n_img
+            assigned(i, assigned_label(i)) = 1;
+            correct(i, assigned_label(i)) = 1;            
+        end
+        assigned_action = assigned;
+        correct_label = correct;        
+    else
+        if ~isempty(map_sub2sup)        
+            fprintf('Results for subclasses:\n');    
+        end
+        correct_labels = cat(1, images(:).actions);
+        table = confusion_table(correct_labels,assigned_action);  
+        accuracy = display_multiclass_accuracy(subclasses, table);
+        precision = display_precision_recall(subclasses, correct_labels, score);     
     end
-    correct_labels = cat(1, images(:).actions);
-    table = confusion_table(correct_labels,assigned_action);  
-    accuracy = display_multiclass_accuracy(subclasses, table);
-    precision = display_precision_recall(subclasses, correct_labels, score); 
-    
+        
     if ~isempty(map_sub2sup)
         fprintf('Results for classes:\n');
         [new_score new_correct_action new_assigned_action] = convert2supclasses(map_sub2sup, score, correct_labels, assigned_action);
